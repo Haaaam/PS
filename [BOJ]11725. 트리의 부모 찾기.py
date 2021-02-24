f0461788f4a0d79e0e5dfca6fc86126b36f99acd
Ham <42646583+imeamin@users.noreply.github.com>
2021-02-24 15:51:34 +0900

    Add files via upload

---
+++ [BOJ]11725. 트리의 부모 찾기.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(100000)
+
+n=int(input())
+graph=[[] for _ in range(n+1)]
+
+parent=[0 for _ in range(n+1)]
+parent[1]=1
+
+for _ in range(n-1):
+    a,b=map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(v):
+    for i in graph[v]:
+        if parent[i]==0:
+            parent[i]=v
+            dfs(i)
+dfs(1)
+for i in parent[2:]:
+    print(i)
+
+
+
