6a99f3611ef69b7d759df2abcc693da7450b7c32
imeamin <hyemin0057@gmail.com>
2021-05-23 19:39:16 +0900

    2021.05.23 HackerRank PS

---
+++ HackerRank/Implementation/[Easy]Subaaray Division.py
@@ -0,0 +1,47 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+def birthday(s, d, m):
+    # Write your code here
+    global n
+    cnt=0
+    if n==1:
+        if s[0]==d:cnt+=1
+        else:cnt=0
+    else:
+        for i in range(1,n+1):
+            if d==sum(s[i-m:i]):
+                cnt+=1
+    return cnt
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
