e9080bc17f76d77eb913561008dcdcdd1e9cdd21
Haaaam <hyemin0057@gmail.com>
2021-05-30 15:21:12 +0900

    2021.05.29 HackerRank Day 14 Scope PS

--- HackerRank/30 Days of Code/Day14 Scope.py
+++ HackerRank/30 Days of Code/Day14 Scope.py
@@ -1,5 +1,11 @@
 class Difference:
-    
+    def __init__(self,a):
+        self.__elements=a
+        self.maximumDifference=0
+
+    def computeDifference(self):
+        self.maximumDifference=max(self.__elements)-min(self.__elements)
+
 n=int(input())
 a=list(map(int,input().split()))
 
