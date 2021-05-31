cec30128ed0e6b7d35932e5f459ad82ac711cb3d
Haaaam <hyemin0057@gmail.com>
2021-05-31 10:01:58 +0900

    2021.05.31 HackerRank Day16 PS

--- HackerRank/30 Days of Code/Day 15 Linked List.py
+++ HackerRank/30 Days of Code/Day 15 Linked List.py
@@ -11,14 +11,6 @@ class Solution:
             current=current.next
 
     def insert(self,head,data):
-        if head is None:
-            head=Node(data)
-            self.tail=head
-        else:
-            node=Node(data)
-            self.tail.next=node
-            self.tail=node
-        return head
 
 
 
