bf4e73b92bb577c6d3244c6f8f491a44d28442a3
Haaaam <hyemin0057@gmail.com>
2021-06-02 00:09:48 +0900

    2021.06.02 HackerRank PS

--- HackerRank/Strings/[Easy]Strong Password.py
+++ HackerRank/Strings/[Easy]Strong Password.py
@@ -1,8 +1,16 @@
-def minimumNumber(n,password):
+def minimumNumber(n, password):
+    # Return the minimum number of characters to make the password strong
+    special_characters='!@#$%^&*()-+'
     cnt=0
-    if n<6:
-
-    return cnt
-
+    if any(i.isdigit() for i in password)==False:
+        cnt+=1
+    if any(i.islower() for i in password)==False:
+        cnt+=1
+    if any(i.isupper() for i in password)==False:
+        cnt+=1
+    if any(i in special_characters for i in password)==False:
+        cnt+=1
+    return max(cnt,6-n)
 n=int(input())
 password=input()
+print(minimumNumber(n,password))
