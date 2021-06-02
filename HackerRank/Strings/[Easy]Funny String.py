8f5b6770ae66efc8b0ff691eda2c29f500f85c51
Haaaam <hyemin0057@gmail.com>
2021-06-02 18:36:22 +0900

    2021.06.02 HackerRank String PS

--- HackerRank/Strings/[Easy]Funny String.py
+++ HackerRank/Strings/[Easy]Funny String.py
@@ -1,17 +1,21 @@
-def funnyString(s):
-    tmp=[]
-    for i in s:
-        tmp.append(ord(i))
-    reversed(tmp)
+'''q=int(input())
+for i in range(q):
+    s=input()
+    tmp=[ord(j) for j in s ]
     ans=[]
-    for i in range(1,len(tmp)):
-        ans.append(abs(tmp[i]-tmp[i-1]))
     cnt=0
-    ans.sort()
-    for i in range(1,len(ans)):
-        if ans[i]==ans[i-1]:
-            cnt+=1
-    if cnt!=0:
+    for k in range(1,len(tmp)):
+        ans.append(abs(tmp[k]-tmp[k-1]))
+    if list(reversed(ans))==ans:
+        print("Funny")
+    else:
+        print("Not Funny")'''
+def funnyString(s):
+    tmp=[ord(j) for j in s]
+    ans=[]
+    for k in range(1,len(tmp)):
+        ans.append(abs(tmp[k]-tmp[k-1]))
+    if list(reversed(ans))==ans:
         return "Funny"
     else:
         return "Not Funny"
@@ -19,3 +23,4 @@ q=int(input())
 for i in range(q):
     s=input()
     print(funnyString(s))
+
