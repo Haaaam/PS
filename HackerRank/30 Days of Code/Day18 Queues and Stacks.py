01d647577c8555080e0d3c58d9532950a75d0c36
Haaaam <hyemin0057@gmail.com>
2021-06-05 22:31:27 +0900

    HackerRank ps

--- HackerRank/30 Days of Code/Day18 Queues and Stacks.py
+++ HackerRank/30 Days of Code/Day18 Queues and Stacks.py
@@ -1,6 +1,12 @@
 import sys
 
 class Solution:
+    def __init__(self):
+        self.
+    def pushCharacter(self,char):
+    def enqueueCharacter(self,char):
+    def popCharacter(self,):
+    def dequeueCharacter(self):
 
 s=input()
 obj=Solution()
@@ -10,5 +16,14 @@ for i in range(len(l)):
     obj.enqueueCharacter(s[i])
 
 isPalindrome=True
+
+for i in range(l//2):
+    if obj.popCharacter()!=obj.dequeueCharacter():
+        isPalindrome=False
+        break
+if isPalindrome:
+    print("The word, "+s+", is a palindrome.")
+else:
+    print("The word, "+s+", is not a palindrome.")
 
 
