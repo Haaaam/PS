4dbc4d95f9533db36a0239168d5ba884d75115c7
Haaaam <hyemin0057@gmail.com>
2021-06-08 10:10:07 +0900

    HackerRank Day21 Generics PS

--- HackerRank/30 Days of Code/Day21 generics.java
+++ HackerRank/30 Days of Code/Day21 generics.java
@@ -1,75 +1,44 @@
-import java.util. *;
+import java.util.*;
 
-class Printer < T > {
+class Printer <T> {
 
-/ **
-* Method Name: printArray
-               * Print
+    /**
+    *    Method Name: printArray
+    *    Print each element of the generic array on a new line. Do not return anything.
+    *    @param A generic array
+    **/
 
-
-each
-element
-of
-the
-generic
-array
-on
-a
-new
-line.Do
-not
-return anything.
-* @ param
-A
-generic
-array
-** /
-
-// Write
-your
-code
-here
-public
-void
-printArray(T[]
-inputArray)
-{
-for (T e: inputArray){
-    System.out.println(e);
-}
-}
+    // Write your code here
+    public void printArray(T[] inputArray)
+    {
+        for (T e : inputArray){
+            System.out.println(e);
+        }
+    }
 }
 
-public
+public class Generics {
 
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
 
-class Generics {
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
 
-public static void main(String args[]){
-Scanner scanner = new Scanner(System.in );
-int n = scanner.nextInt();
-Integer[] intArray = new Integer[n];
-for (int i = 0; i < n; i++) {
-intArray[i] = scanner.nextInt();
-}
-
-n = scanner.nextInt();
-String[] stringArray = new String[n];
-for (int i = 0; i < n; i++) {
-stringArray[i] = scanner.next();
-}
-
-Printer < Integer > intPrinter = new Printer < Integer > ();
-Printer < String > stringPrinter = new Printer < String > ();
-intPrinter.printArray( intArray  );
-stringPrinter.printArray( stringArray );
-if (Printer.
-
-
-class .getDeclaredMethods().length > 1){
-
-
-System.out.println("The Printer class should only have 1 method named printArray.");
-}
-}
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    }
 }
