519b3ac297f9ca5fedd8bd64520e35684d733afb
Haaaam <hyemin0057@gmail.com>
2021-06-15 21:15:53 +0900

    HackerRank Implementation PS

--- HackerRank/Implementation/[Easy]Library Fine.py
+++ HackerRank/Implementation/[Easy]Library Fine.py
@@ -1,6 +1,20 @@
 def libraryFine(d1,m1,y1,d2,m2,y2):
+    res=0
+    if y1>y2:
+        res=10000
+    elif y1==y2:
+        if m1>m2:
+            res=500*(m1-m2)
+        elif m1==m2:
+            if d1>d2 and (m1-m2)>=0 and (y1-y2)>=0:
+                res=15*(d1-d2)
+            else:
+                res=0
+    return res
+
+
 
 
 d1,m1,y1=map(int,input().split())
 d2,m2,y2=map(int,input().split())
-
+print(libraryFine(d1,m1,y1,d2,m2,y2))
