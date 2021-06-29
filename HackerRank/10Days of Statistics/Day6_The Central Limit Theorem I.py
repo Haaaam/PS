f77f3833470cb7335fbeadbf273115204abd64ee
korband <hyemin0057@gmail.com>
2021-06-29 13:36:57 +0900

    The Central Limit Theorem

---
+++ HackerRank/10Days of Statistics/Day6_The Central Limit Theorem I.py
@@ -0,0 +1,16 @@
+#Central Limit Theorem
+import math
+
+x,boxes,mu,sd=int(input()),int(input()),int(input()),int(input())
+
+mu_sum=boxes*mu
+
+sd_sum=math.sqrt(boxes)*sd
+
+def cdf(x,mu,sd):
+    Z=(x-mu)/sd
+    return 0.5*(1+math.erf(Z/(math.sqrt(2))))
+
+print(round(cdf(x,mu_sum,sd_sum),4))
+
+
