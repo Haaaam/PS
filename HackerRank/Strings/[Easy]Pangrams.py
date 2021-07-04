8e05b51011e217f7f661e66110b1bcc32c6b3f18
Haaaam <hyemin0057@gmail.com>
2021-07-04 16:51:01 +0900

    Strings PS

---
+++ HackerRank/Strings/[Easy]Pangrams.py
@@ -0,0 +1,8 @@
+def sol(s):
+   alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+   return set(alphabet)<set(s.lower())
+s=input()
+if sol(s):
+    print("pangram")
+else:
+    print("not pangram")
