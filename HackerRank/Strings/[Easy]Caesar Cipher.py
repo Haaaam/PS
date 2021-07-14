7eac5b3e6715598564777747c0edc097634adba6
Haaaam <hyemin0057@gmail.com>
2021-07-14 10:35:05 +0900

    String PS

--- HackerRank/Strings/[Easy]Caesar Cipher.py
+++ HackerRank/Strings/[Easy]Caesar Cipher.py
@@ -3,17 +3,19 @@ s=input()
 k=int(input())
 ans=''
 for i in s:
+    if ord(i)>=97 and ord(i)<=122:
+        if ord(i)+k%26>122:
 
+            ans+=chr(96+(ord(i)+k%26-122))
 
-    if i.isalpha():
-        if i.islower():
-            tmp=ord(i)
-            if tmp>=122:
-                tmp=96
-            tmp+=k
-            ans+=chr(tmp)
         else:
-            
+            ans+=chr(ord(i)+k%26)
+
+    elif ord(i)>=65 and ord(i)<=90:
+        if ord(i)+k%26>90:
+            ans+=chr(64+ord(i)+k%26-90)
+        else:
+            ans+=chr(ord(i)+k%26)
     else:
         ans+=i
 print(ans)
