b2334f72ca0eb51dd493c64980ae834b3efa4442
Haaaam <hyemin0057@gmail.com>
2021-07-21 22:21:24 +0900

    Implementation PS

--- HackerRank/Implementation/[Easy]Fair Rations.py
+++ HackerRank/Implementation/[Easy]Fair Rations.py
@@ -1,3 +1,13 @@
-
+def sol(b):
+    cnt=0
+    for i in range(len(b)):
+        try:
+            if b[i]%2==1:
+                cnt+=2
+                b[i+1]+=1
+        except:
+            return 'NO'
+    return cnt
 n=int(input())
 b=list(map(int,input().split()))
+print(sol(b))
