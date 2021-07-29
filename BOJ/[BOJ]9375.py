e68cd5e691dfbac2215ad1e4b8f75cc360a2824a
Haaaam <hyemin0057@gmail.com>
2021-07-29 10:45:28 +0900

    [String]패션왕 신해빈

---
+++ BOJ/[BOJ]9375.py
@@ -0,0 +1,22 @@
+def sol(n,wear,res):
+    if n>0:
+        for j in range(n):
+            cloth,type=input().split()
+
+            if type in wear:
+                wear[type]+=1
+            else:
+                wear[type]=1
+
+        for key,val in wear.items():
+            res*=(val+1)
+           
+    return res-1
+
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    wear=dict()
+    res=1
+    print(sol(n,wear,res))
+
