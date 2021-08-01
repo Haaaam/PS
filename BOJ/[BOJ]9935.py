056f00a9fadd264bd4008de5a2afc4b3f7aad050
Haaaam <hyemin0057@gmail.com>
2021-08-01 18:29:16 +0900

    [String]문자열 폭발

--- BOJ/[BOJ]9935.py
+++ BOJ/[BOJ]9935.py
@@ -1,11 +1,10 @@
 s=input()
 target=input()
-last=target[-1]
 stack=[]
 
 for i in range(len(s)):
     stack.append(s[i])
-    if s[i]==last and ''.join(stack[-len(target):])==target:
+    if s[i]==target[-1] and ''.join(stack[-len(target):])==target:
         del stack[-len(target):]
 
 if stack:
