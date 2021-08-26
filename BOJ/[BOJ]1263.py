1ed441e477b88bd67c3715e1bfddfc6babbdbcdd
Haaaam <hyemin0057@gmail.com>
2021-08-26 18:52:19 +0900

    [BOJ]시간관리

--- BOJ/[BOJ]1263.py
+++ BOJ/[BOJ]1263.py
@@ -1,11 +1,14 @@
-# 시간 관리
+# 시간 관리(다시풀기)
 n=int(input())
-time=[]
-for i in range(n):
-    t,s=map(int,input().split())
-    time.append(s-t)
-time=sorted(time)
-if time[0]<0:
+time=[list(map(int,input().split())) for _ in range(n)]
+time.sort(key=lambda x:x[1],reverse=True)
+s=time[0][1]-time[0][0]
+for i in range(1,n):
+    if s>time[i][1]:
+        s=time[i][1]-time[i][0]
+    else:
+        s-=time[i][0]
+if s<0:
     print(-1)
 else:
-    print(time[0])
+    print(s)
