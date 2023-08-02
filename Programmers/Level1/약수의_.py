52748d72a477fa3893c4e3b2df3cefacc5e46e7e
Hyemin Lim <42646583+Haaaam@users.noreply.github.com>
2023-08-02 11:26:24 +0900

    [Level1]약수의 합

---
+++ Programmers/Level1/약수의_.py
@@ -0,0 +1,13 @@
+def solution(n):
+    ans=0
+    for i in range(1,n+1):
+        if n%i==0:
+            ans+=i
+    return ans
+
+n=12
+ans=0
+for i in range(1,n+1):
+    if n%i==0:
+        ans+=i
+print(ans)
