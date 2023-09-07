1627fcc5c8ef55fe706ed91611fe439dce509c39
Hyemin Lim <42646583+Haaaam@users.noreply.github.com>
2023-09-07 09:55:15 +0900

    [Level0]머쓱이보다 키 큰 사람

---
+++ Programmers/basic/머쓱이보다_키_큰_사.py
@@ -0,0 +1,14 @@
+def solution(array, height):
+    answer=0
+    for i in array:
+        if height < i:
+            answer += 1
+    return answer
+
+array=[149,180,192,170]
+height=167
+answer=0
+for i in array:
+    if height <i:
+        answer+=1
+print(answer)
