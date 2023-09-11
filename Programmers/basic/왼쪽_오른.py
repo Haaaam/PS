98cdabe6515f9c34e7d9bb16a52a29ea5d3d4f51
Hyemin Lim <42646583+Haaaam@users.noreply.github.com>
2023-09-11 23:57:13 +0900

    [Level 0] 왼쪽 오른쪽

---
+++ Programmers/basic/왼쪽_오른.py
@@ -0,0 +1,17 @@
+def solution(str_list):
+    answer=[]
+    idx=0
+    for i in str_list:
+        if i=='l':
+            answer=str_list[:idx]
+            break
+        elif i=="r":
+            answer=str_list[idx+1:]
+            break
+        idx+=1
+
+    return answer
+
+str_list=["l"]
+
+print(solution(str_list))
