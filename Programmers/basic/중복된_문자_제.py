ac2c5bcd5aa5e8a7a0ce9b6f0e158e26e5fa92d2
Hyemin Lim <42646583+Haaaam@users.noreply.github.com>
2023-09-11 10:57:05 +0900

    [Level 0]중복된 문자 제거

---
+++ Programmers/basic/중복된_문자_제.py
@@ -0,0 +1,12 @@
+def solution(my_string):
+    answer=''
+    return answer
+
+my_string="people"
+answer=''
+for i in my_string:
+    if i in answer:
+        pass
+    else:
+        answer+=i
+print(answer)
