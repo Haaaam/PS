57324aeddfa81b0069076e2774731fac5e339809
Hyemin Lim <42646583+Haaaam@users.noreply.github.com>
2023-09-15 09:15:36 +0900

    [Level 0]배열_만들기3

---
+++ Programmers/basic/배열_만들기_3.py
@@ -0,0 +1,12 @@
+def solution(arr,intervals):
+    answer=[]
+    for i in intervals:
+        answer += arr[i[0]:i[1] + 1]
+    return answer
+
+arr=[1,2,3,4,5]
+intervals=[[1,3],[0,4]]
+answer=[]
+for i in intervals:
+    answer+=arr[i[0]:i[1]+1]
+print(answer)
