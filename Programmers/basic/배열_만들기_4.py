45c5a4eab4e635c12161a56ebc79425a9df12476
Hyemin Lim <42646583+Haaaam@users.noreply.github.com>
2023-09-15 10:46:04 +0900

    [Level 0] 배열 만들기 4

---
+++ Programmers/basic/배열_만들기_4.py
@@ -0,0 +1,34 @@
+def solution(arr):
+    stk = []
+    i = 0
+    while i < len(arr):
+
+        if len(stk) == 0:
+            stk.append(arr[i])
+            i += 1
+        else:
+            if stk[-1] < arr[i]:
+                stk.append(arr[i])
+                i += 1
+            elif stk[-1] >= arr[i]:
+                stk.pop()
+    return stk
+
+arr=[1,4,2,5,3]
+stk=[]
+i=0
+while i<len(arr):
+
+    if len(stk) == 0:
+        stk.append(arr[i])
+        i+=1
+    else:
+        if stk[-1] < arr[i]:
+            stk.append(arr[i])
+            i+=1
+        elif stk[-1] >= arr[i]:
+            stk.pop()
+
+
+    print(i,stk)
+print(stk)
