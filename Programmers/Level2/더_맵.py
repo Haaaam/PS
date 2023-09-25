eb94cf3a222881ff717b4abe517ad3b6614bb9c4
Hyemin Lim <42646583+Haaaam@users.noreply.github.com>
2023-09-25 22:28:10 +0900

    [Level 2]더 맵게

--- Programmers/Level2/더_맵.py
+++ Programmers/Level2/더_맵.py
@@ -20,19 +20,4 @@ def solution(scoville,K):
 # 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞기
 scoville=[2,1,3,9,10,12]
 K=7
-taste=0
-cnt=0
-heap=[]
-for i in scoville:
-    heapq.heappush(heap,i)
-
-while heap[0]<K:
-    taste=heapq.heappop(heap)+(heapq.heappop(heap)*2)
-    heapq.heappush(heap,taste)
-    cnt+=1
-    if scoville[0]<K and len(heap)==1:
-        cnt=-1
-        break
-
-    print(heap)
-print(cnt)
+print(solution(scoville,K))
